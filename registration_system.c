5be6aa6c9f6cb659c5b185309c330a6a70d4b237
Inna Khachikyan <khachikyaninna@gmail.com>
2025-12-01 12:19:26 +0400

    Implemented, needs testing

--- registration_system.c
+++ registration_system.c
@@ -55,3 +55,64 @@ static const char *STUDENT_IDS[] = {
 
 bool token_generated[NUM_STUDENTS] = {false};
 
+static int find_student_index(const char *id) {
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        if (strcmp(STUDENT_IDS[i], id) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+static bool all_students_done(void) {
+    for (int i = 0; i < NUM_STUDENTS; i++) {
+        if (!token_generated[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+static void free_keys(void) {
+    if (g_public_n) {
+        BN_free(g_public_n);
+        g_public_n = NULL;
+    }
+    if (g_public_e) {
+        BN_free(g_public_e);
+        g_public_e = NULL;
+    }
+    if (s_private_d) {
+        BN_free(s_private_d);
+        s_private_d = NULL;
+    }
+}
+
+
+int system_blind_sign(const BIGNUM *m_blinded, BIGNUM **s_blinded_out) {
+    if (!m_blinded || !s_private_d || !g_public_n || !s_blinded_out) {
+        return 0;
+    }
+
+    BN_CTX *ctx = BN_CTX_new();
+    if (!ctx) {
+        return 0;
+    }
+
+    BIGNUM *s = BN_new();
+    if (!s) {
+        BN_CTX_free(ctx);
+        return 0;
+    }
+
+    if (!rsa_decrypt(m_blinded, g_public_n, s_private_d, s, ctx)) {
+        BN_free(s);
+        BN_CTX_free(ctx);
+        return 0;
+    }
+
+    *s_blinded_out = s;
+    BN_CTX_free(ctx);
+    return 1;
+}
+
